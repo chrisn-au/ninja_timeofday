90825d5db8a6ea74d74ce557b77ccbce39bf585d
Demo User <ubuntu@ninjablock.(none)>
2014-01-27 20:50:03 +1100

    Added default config

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ function myModule(opts,app) {
   var self = this;
   this.first = true;
   this.opts = opts;
-  opts.Locations = opts.Locations || [];
+  
+  opts.Locations = opts.Locations || new Array(defaultConfig());
   self.save();
 
   app.on('client::up',function(){
@@ -48,3 +49,18 @@ myModule.prototype.config = function(config) {
 
 // Export it
 module.exports = myModule;
+
+function defaultConfig()
+{
+   var day = { "name" : "sunriseEnd" , "display" : "DAY", "offset" : -150 } 
+   var dawn = { "name" : "dawn", "display" : "DAWN" }
+   var sunset = { "name" : "sunset", "display" : "SUNSET" }
+   var night = { "name" : "dusk", "display" : "NIGHT" } 
+   var dusk = { "name" : "mydusk", "display" : "DUSK" , "tod" : "PM" , "angle" : 5} 
+
+   var tod = new Array(dawn,day,sunset,dusk,night)
+   var top = { "name" : "NinjaHQ", "latitude" : -33.8600, "longitude" : 151.211 , "Times" : tod}
+  
+   return top;
+   
+ }
